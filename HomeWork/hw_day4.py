3c20e29e52238f7fb0bc91e1749e6fcbde10f87a
Vyshnav <vyshnav2004sk@gmail.com>
2026-01-02 18:01:12 +0530

    hw day4

--- HomeWork/hw_day4.py
+++ HomeWork/hw_day4.py
@@ -1,6 +1,6 @@
 webdevelopment = ['appu' , 'kichu', 'sachu']
 ui_ux_design = ['ram', 'tara', 'raj']
-data_science = ['sagar', 'jacky', 'kattappa']
+data_science = ['arsha', 'jacky', 'kattappa']
 
 all_participants = [webdevelopment, ui_ux_design, data_science]
 print(all_participants)
@@ -20,3 +20,12 @@ print(data_science_new)
 
 print(webdevelopment[:2])
 
+name_len_list = [len(i) for i in data_science_new]
+print(name_len_list)
+
+print(f"Is arsha enrolled: {'arsha' in webdevelopment or 'arsha' in ui_ux_design or 'arsha' in data_science_new}")
+
+name_tuple = []
+name_tuple.extend([webdevelopment[0:1], ui_ux_design[0:1], data_science_new[:1]])
+name_tuple = tuple(name_tuple)
+print(name_tuple)
