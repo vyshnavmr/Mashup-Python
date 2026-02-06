6816e563ad4626b6610144cd82aa8e6ffe73a1cc
Vyshnav <vyshnav2004sk@gmail.com>
2026-02-06 19:59:42 +0530

    changes made

--- ClassWork/cwDay10.py
+++ ClassWork/cwDay10.py
@@ -1,34 +1,29 @@
 class User:
+    def __init__(self, name, joining_year):
+        self.name = name
+        self.joining_year = joining_year
+
     def years_on_platform(self):
         return 2025 - self.joining_year
 
-    def show_info(self):
-        print("Name:", self.name)
-        print("Role:", self.role())
-        print("Years on platform:", self.years_on_platform())
-        print()
 
 
 class Customer(User):
-    def role(self):
-        return "Customer"
+    
+    def show_info(self):
+        years = self.years_on_platform()
+        print(f"\n{self.name} is a Customer and has used the platform for {years} years")
 
 
 class Vendor(User):
-    def role(self):
-        return "Vendor"
+    
+    def show_info(self):
+        years = self.years_on_platform()
+        print(f"{self.name} is a Vendor and has used the platform for {years} years\n")
+    
 
-
-customer1 = Customer()
-customer1.name = "Alice"
-customer1.joining_year = 2020
-
-
-vendor1 = Vendor()
-vendor1.name = "Bob"
-vendor1.joining_year = 2018
-
+customer1 = Customer('Alice', 2020)
+vendor1 = Vendor('rahul', 2022)
 
 customer1.show_info()
 vendor1.show_info()
-   
